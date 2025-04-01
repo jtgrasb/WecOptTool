fb85a6e2958e33cbe99897dce6db031f85f2619b
jtgrasb <87095491+jtgrasb@users.noreply.github.com>
2025-04-01 13:23:12 -0600

    Update mass matrix

--- examples/WEC-Sim/userDefinedFunctions.m
+++ examples/WEC-Sim/userDefinedFunctions.m
@@ -89,21 +89,88 @@ ylabel('Mech power (W)')
 
 % What if I run BEM with only 2 points like with WOT?
 
-% plots the forces on body 1 converted to be about pto 1
-excF1 = output.bodies(1).forceExcitation(:,3)*5 + output.bodies(1).forceExcitation(:,5);
-ptoF1 = output.ptos(1).forceInternalMechanics(:,5);
-radF1 = (output.bodies(1).forceRadiationDamping(:,3)+output.bodies(1).forceAddedMass(:,3))*5 + (output.bodies(1).forceRadiationDamping(:,5)+output.bodies(1).forceAddedMass(:,5));
-hsF1 = output.bodies(1).forceRestoring(:,3)*5 + output.bodies(1).forceRestoring(:,5);
-
-figure()
-plot(output.bodies(1).time, excF1)
-
-figure()
-plot(output.bodies(1).time, ptoF1)
-
-figure()
-plot(output.bodies(1).time, radF1)
-
-figure()
-plot(output.bodies(1).time, hsF1)
-
+% % plots the forces on body 1 converted to be about pto 1
+% excF1 = output.bodies(1).forceExcitation(:,3)*5 + output.bodies(1).forceExcitation(:,5);
+% ptoF1 = output.ptos(1).forceInternalMechanics(:,5);
+% radF1 = (output.bodies(1).forceRadiationDamping(:,3)+output.bodies(1).forceAddedMass(:,3))*5 + (output.bodies(1).forceRadiationDamping(:,5)+output.bodies(1).forceAddedMass(:,5));
+% hsF1 = output.bodies(1).forceRestoring(:,3)*5 + output.bodies(1).forceRestoring(:,5);
+% 
+% figure()
+% plot(output.bodies(1).time, excF1)
+% ylabel('Excitation (Nm) PTO 1')
+% 
+% % figure()
+% % plot(output.bodies(1).time, ptoF1)
+% 
+% figure()
+% plot(output.bodies(1).time, radF1)
+% ylabel('Radiation (Nm) PTO 1')
+% 
+% figure()
+% plot(output.bodies(1).time, hsF1)
+% ylabel('Hydrostatics (Nm) PTO 1')
+% 
+% 
+% % plots the forces on body 2 converted to be about pto 2
+% excF2 = output.bodies(2).forceExcitation(:,3)*5 + output.bodies(2).forceExcitation(:,5);
+% ptoF2 = output.ptos(2).forceInternalMechanics(:,5);
+% radF2 = (output.bodies(2).forceRadiationDamping(:,3)+output.bodies(2).forceAddedMass(:,3))*5 + (output.bodies(2).forceRadiationDamping(:,5)+output.bodies(2).forceAddedMass(:,5));
+% hsF2 = output.bodies(2).forceRestoring(:,3)*5 + output.bodies(2).forceRestoring(:,5);
+% 
+% figure()
+% plot(output.bodies(2).time, excF2)
+% ylabel('Excitation (Nm) PTO 2')
+% 
+% % figure()
+% % plot(output.bodies(1).time, ptoF1)
+% 
+% figure()
+% plot(output.bodies(2).time, radF2)
+% ylabel('Radiation (Nm) PTO 2')
+% 
+% figure()
+% plot(output.bodies(2).time, hsF2)
+% ylabel('Hydrostatics (Nm) PTO 2')
+% 
+% 
+% %% plots of PTO torque extractions
+% 
+% figure()
+% plot(forceB1)
+% ylabel('Constraint forces B1 (N)')
+% legend()
+% 
+% figure()
+% plot(forceB1Tot)
+% ylabel('Total forces B1 (N)')
+% legend()
+% 
+% figure()
+% plot(torqueB1)
+% ylabel('Constraint torques B1 (Nm)')
+% legend()
+% 
+% figure()
+% plot(torqueB1Tot)
+% ylabel('Total torques B1 (Nm)')
+% legend()
+% 
+% figure()
+% plot(forceB2)
+% ylabel('Constraint forces B2 (N)')
+% legend()
+% 
+% figure()
+% plot(forceB2Tot)
+% ylabel('Total forces B2 (N)')
+% legend()
+% 
+% figure()
+% plot(torqueB2)
+% ylabel('Constraint torques B2 (Nm)')
+% legend()
+% 
+% figure()
+% plot(torqueB2Tot)
+% ylabel('Total torques B2 (Nm)')
+% legend()
--- examples/WEC-Sim/wecSimInputFile.m
+++ examples/WEC-Sim/wecSimInputFile.m
@@ -17,7 +17,7 @@ simu.rho = 1025;
 
 % % Regular Waves  
 waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                 
-waves.height = .5;                     % Wave Height [m]
+waves.height = .01;                     % Wave Height [m]
 waves.period = 7.4371;                       % Wave Period [s]
 
 % % Regular Waves with CIC
@@ -91,10 +91,10 @@ constraint(3).location = [-15 0 0];               % Constraint Location [m]
 % Translational PTO
 pto(1) = ptoClass('PTO1');                      % Initialize PTO Class for PTO1
 pto(1).stiffness = 0;                           % PTO Stiffness [N/m]
-pto(1).damping = 1e4;                          % PTO Damping [N/(m/s)]
+pto(1).damping = 0;                          % PTO Damping [N/(m/s)]
 pto(1).location = [0 0 0];                      % PTO Location [m]
 
 pto(2) = ptoClass('PTO2');                      % Initialize PTO Class for PTO1
 pto(2).stiffness = 0;                           % PTO Stiffness [N/m]
-pto(2).damping = 1e4;                          % PTO Damping [N/(m/s)]
+pto(2).damping = 0;                          % PTO Damping [N/(m/s)]
 pto(2).location = [-10 0 0];                      % PTO Location [m]
