b06651a60e67e0d89b36298c1e1d4b6589815889
jtgrasb <87095491+jtgrasb@users.noreply.github.com>
2025-10-30 16:36:35 -0600

    get both models working

--- attenuator/WEC-Sim/userDefinedFunctions.m
+++ attenuator/WEC-Sim/userDefinedFunctions.m
@@ -1,4 +1,5 @@
 %Example of user input MATLAB file for post processing
+close all
 
 %Plot waves
 waves.plotElevation(simu.rampTime);
@@ -13,13 +14,27 @@ output.plotResponse(1,3);
 %Plot heave response for body 2
 output.plotResponse(2,3);
 
-%Plot heave forces for body 1
-output.plotForces(1,3);
+% plot hinge motions and relate to heave
+figure()
+plot(output.ptos(1).time,output.ptos(1).position(:,5)*180/pi)
+hold on
+plot(output.ptos(2).time,output.ptos(2).position(:,5)*180/pi)
+xlabel('time (s)')
+ylabel('pto rotation (deg)')
+legend('pto 1','pto 2')
 
-%Plot heave forces for body 2
-output.plotForces(2,3);
+bodyLocsX = [1 3];
+armLength = -pto(2).location(1);
 
-%Save waves and response as video
-% output.saveViz(simu,body,waves,...
-%     'timesPerFrame',5,'axisLimits',[-150 150 -150 150 -50 20],...
-%     'startEndTime',[100 125]);
+theoreticalHeave1 = bodyLocsX(1)*sin(output.ptos(1).position(:,5));
+theoreticalHeave2 = armLength*sin(output.ptos(1).position(:,5)) + (bodyLocsX(2)-armLength)*sin(output.ptos(1).position(:,5) + output.ptos(2).position(:,5));
+
+figure()
+plot(output.bodies(1).time,output.bodies(1).position(:,3))
+hold on
+plot(output.ptos(2).time,output.bodies(2).position(:,3))
+plot(output.ptos(2).time,theoreticalHeave1,'--')
+plot(output.ptos(2).time,theoreticalHeave2,'--')
+xlabel('time (s)')
+ylabel('position (m)')
+legend('body 1 heave','body 2 heave','body 1 theor','body 2 theor')
